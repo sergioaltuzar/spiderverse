2f03c149a8c0e37abc5cfb502c5a5b126a96200d
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 13:59:07 -0500

    modificando prueba

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+describe("Unit Test for Spiderman class", () => {
+    test('1 Create an spiderman object', () => {
+     //Aqui escribimos el código que queremos usar tal cual
+     //Quiero poder instanciar un objeto Spiderman con esta informaciones
+     const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony");
+     // Validamos que este codigo funcione de la forma esperada
+     expect(andrewGarfield.name).toBe("Spiderman Sony");
+     expect(andrewGarfield.age).toBe(31);
+     expect(andrewGarfield.actor).toBe("Andrew Garfield");
+     expect(andrewGarfield.movies).toBe(31);
+     expect(andrewGarfield.studio).toBe(Sony);
+
     });
   })
