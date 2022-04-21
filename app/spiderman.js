08503813d011c0c9ae652fd0590ab6dca4f11512
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-04-21 14:06:46 -0500

    agregando atributos

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,3 +1,7 @@
 class Spiderman{constructor(name){this.name=name}};
+class Spiderman{constructor(age){this.name=age}};
+class Spiderman{constructor(actor){this.name=actor}};
+class Spiderman{constructor(movies){this.name=movies}};
+class Spiderman{constructor(studio){this.name=studio}};
 
 module.exports = Spiderman;
